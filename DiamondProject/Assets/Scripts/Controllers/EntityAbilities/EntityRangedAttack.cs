f20cc4d136f31a3d5ab6315a632b8df2c7bf26f1
Wizerdy <55747980+Wizerdy@users.noreply.github.com>
2022-03-04 15:16:18 +0100

    GD UwU

--- DiamondProject/Assets/Scripts/Controllers/EntityAbilities/EntityRangedAttack.cs
+++ DiamondProject/Assets/Scripts/Controllers/EntityAbilities/EntityRangedAttack.cs
@@ -5,6 +5,7 @@ using ToolsBoxEngine;
 
 public class EntityRangedAttack : MonoBehaviour {
     [SerializeField] GameObject _bullet;
+    [SerializeField] int _damage;
     [SerializeField] float _bulletSpeed;
     [SerializeField] float _rangedAttackCooldown = 1f;
 
@@ -18,6 +19,7 @@ public class EntityRangedAttack : MonoBehaviour {
         OnAttack?.Invoke(direction);
 
         GameObject bull = Instantiate(_bullet, transform.position, Quaternion.identity);
+        bull.GetComponent<AttackHitbox>().damage = _damage;
         bull.GetComponent<Rigidbody2D>().velocity = direction * _bulletSpeed;
         _canRangeAttack = false;
         StartCoroutine(Tools.Delay(() => _canRangeAttack = true, _rangedAttackCooldown));
