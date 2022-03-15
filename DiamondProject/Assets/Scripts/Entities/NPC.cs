6418070384c349e3f206e6b56ecb446aef25b9dd
Dadu <55183284+Darnald0@users.noreply.github.com>
2022-03-15 16:17:33 +0100

    WIP

--- DiamondProject/Assets/Scripts/Entities/NPC.cs
+++ DiamondProject/Assets/Scripts/Entities/NPC.cs
@@ -11,21 +11,25 @@ public class NPC : Entity {
     public Tools.BasicDelegate<NPC> OnInteract;
     public Tools.BasicDelegate<NPC> OnStopInteract;
 
+    public string flowchartMessage;
+
     void Update() {
 
     }
 
     public void StartTalking(GameObject playerRef) {
         //player = playerRef;
-        //player.GetComponent<OldPlayerController>().disableMovement();
-        flowchart.SetActive(true);
-        //flowchart.enabled = true;
+        //player.GetComponent<PlayerController>().disableMovement();
+        Debug.Log("START CONVERSATION");
+        //flowchart.SetActive(true);
+        Fungus.Flowchart.BroadcastFungusMessage(flowchartMessage);
         OnInteract?.Invoke(this);
     }
 
     public void StopTalking() {
-        flowchart.SetActive(false);
-        //player.GetComponent<OldPlayerController>().enableMovement();
+        Debug.Log("STOP CONVERSATION");
+        //player.GetComponent<PlayerController>().enableMovement();
+        //flowchart.SetActive(false);
         OnStopInteract?.Invoke(this);
     }
 }
