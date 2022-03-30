a5a72baf89480236e21099cd7bd3d4a5a4524162
Adrien Ribeiro <47816763+W3R10S@users.noreply.github.com>
2022-03-30 09:48:46 +0200

    Some feedbacks added
    Boss hit
    Player hit
    Player death
    Health bar particles

--- DiamondProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ DiamondProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID ATTACK = 180661997U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
