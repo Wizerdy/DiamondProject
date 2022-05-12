2ac4c4c2fe810a24b500403e05ac437b219605ad
Wizerdy <55747980+Wizerdy@users.noreply.github.com>
2022-05-12 13:08:28 +0200

    ChangeShape animation place holder

--- DiamondProject/Assets/Scripts/Shape/ChangeBossOnShape.cs
+++ DiamondProject/Assets/Scripts/Shape/ChangeBossOnShape.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ToolsBoxEngine;
 
 public class ChangeBossOnShape : MonoBehaviour {
+    [Header("References")]
     [SerializeField] BossShapeSystem _shapeSystem;
     [SerializeField] IMeetARealBoss _boss;
+    [Header("System")]
+    [SerializeField] float _timeBeforeChange = 0f;
 
     private void Reset() {
         _shapeSystem = GetComponent<BossShapeSystem>();
@@ -20,6 +24,11 @@ public class ChangeBossOnShape : MonoBehaviour {
     }
 
     void _OnEnterShape(BossShape shape) {
+        if (_timeBeforeChange <= 0f) { ChangeShape(shape); return; }
+        StartCoroutine(Tools.Delay(ChangeShape, shape, _timeBeforeChange));
+    }
+
+    public void ChangeShape(BossShape shape) {
         if (_boss == null) { return; }
         if (shape.ColorSwap) {
             _boss.ColorSwap(shape.Red, shape.Green, shape.Blue);
