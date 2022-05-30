f1e39447f9a13b3e85b30bb4804e9d661214748f
Wizerdy <55747980+Wizerdy@users.noreply.github.com>
2022-05-30 11:59:04 +0200

    Thunderstruck !

---
+++ DiamondProject/Assets/Scripts/Boss/Actions/FallingThunder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingThunder : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] DamageHealth _damageHealth = null;
+    [SerializeField] VisualEffect _lightning = null;
+    
+    int _damage = 10;
+
+    private void Start() {
+        _lightning
+    }
+}
