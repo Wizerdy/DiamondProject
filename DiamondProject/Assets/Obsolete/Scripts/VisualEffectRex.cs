cfaa024f5958bdeb33216be9e66d00bd23f9392b
arthur-cable <54852674+arthur-cable@users.noreply.github.com>
2022-05-30 17:09:06 +0200

    Oui.

---
+++ DiamondProject/Assets/Obsolete/Scripts/VisualEffectRex.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualEffectRex : MonoBehaviour {
+    
+    [System.Serializable]
+    struct SpriteColor {
+        public Color color;
+        public float weight;
+        public int id;
+        public SpriteColor(Color color, float weight, int id) {
+            this.color = color;
+            this.weight = weight;
+            this.id = id;
+        }
+    }
+    SpriteRenderer sr;
+    [SerializeField] float damageVisualEffectTime = 0.5f;
+    [SerializeField] List<SpriteColor> colors = new List<SpriteColor>();
+
+    private void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        AddColor(Color.green, 0, 1);
+    }
+    public void AddColor(Color color, float weight, int id) {
+        SpriteColor colorSprite = new SpriteColor(color, weight, id);
+        colors.Add(colorSprite);
+        UpdateColor();
+    }
+
+    public void AddColor(Color color, float weight, float duration) {
+        StartCoroutine(AddTempColor(color, weight, duration));
+    }
+
+    public void RemoveColor(int id) {
+        for (int i = 0; i < colors.Count; i++) {
+            if (colors[i].id == id) { 
+                colors.RemoveAt(i);
+                i--;
+            }
+        }
+        UpdateColor();
+    }
+    IEnumerator AddTempColor(Color color, float weight, float duration = 1) { 
+        SpriteColor colorSprite = new SpriteColor(color, weight, 0);
+        colors.Add(colorSprite);
+        UpdateColor();
+        yield return new WaitForSeconds(duration);
+        colors.Remove(colorSprite);
+        UpdateColor();
+    }
+
+    void UpdateColor() {
+        colors.Sort(BiggerWeightFirst);
+        sr.color = colors[0].color;
+    }
+    int BiggerWeightFirst(SpriteColor a, SpriteColor b) {
+        if (a.weight > b.weight) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+}
--- DiamondProject/Assets/Scripts/Boss/Actions/FallingThunder.cs
+++ DiamondProject/Assets/Scripts/Boss/Actions/FallingThunder.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
+using ToolsBoxEngine;
 
 public class FallingThunder : MonoBehaviour {
     [Header("References")]
     [SerializeField] DamageHealth _damageHealth = null;
     [SerializeField] VisualEffect _lightning = null;
-    
+
+    [Header("Parameters")]
+    [SerializeField] float _height = 0f;
+    [SerializeField] float _delay = 1f;
+
+    Vector3 _target;
     int _damage = 10;
 
+    public void Init(int damage, float delay) {
+        _target = transform.position;
+        _damageHealth?.SetDamage(_damage);
+        StartCoroutine(Tools.Delay(DoDamage, _delay, delay));
+    }
+
     private void Start() {
-        _lightning
+        _damageHealth.gameObject.SetActive(false);
+        _lightning.gameObject.SetActive(false);
+        _lightning.transform.position = transform.position.Override(_height);
+        _lightning?.SetFloat("Height", transform.position.y);
+    }
+
+    private void DoDamage(float time) {
+        StartCoroutine(CollisionTime(time));
+
+        IEnumerator CollisionTime(float time) {
+            _damageHealth.gameObject.SetActive(true);
+            _lightning.gameObject.SetActive(true);
+            yield return new WaitForSeconds(time);
+            _damageHealth.gameObject.SetActive(false);
+            _lightning.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 }
