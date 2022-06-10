ae37cc6ba800d62b247f48e8988d9b5e0b0cfdd5
Dadu <55183284+Darnald0@users.noreply.github.com>
2022-06-10 11:53:31 +0200

    coucou florian

--- DiamondProject/Assets/Scripts/Posterity/PosterityVNValue.cs
+++ DiamondProject/Assets/Scripts/Posterity/PosterityVNValue.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PosterityVNValue : MonoBehaviour
 {
     [SerializeField] private PosterityObject posterityObj;
     [SerializeField] private BossShapeSystem shapeSystem;
+    [SerializeField] private EntityChargeRanged chargeRange;
+
+    private void Start() {
+        chargeRange.OnHit += ArrowHit;
+    }
+    public void ArrowHit(GameObject obj) {
+        Debug.Log("arrow hit");
+        Debug.Log(obj.name);
+        //if (collision.gameObject.GetComponent<ProtectorTree>()) {
+
+        //}
+
+        //if (collision.gameObject.GetComponent<Boomerang>()) {
+
+        //}
+
+        //if (collision.gameObject.GetComponent<LeafBeam>()) {
+
+        //}
+        //if (collision.gameObject.GetComponent<SnowAbsorption>()) {
+
+        //}
+
+    }
+
+    public void SwordHit() {
+        //if (collision.gameObject.GetComponent<shield>) {
+
+        //}
+
+        //if (collision.gameObject.GetComponent<>)
+    }
 
     public void OnTriggerActivate() {
         if (shapeSystem.Shape.Type == Shape.NEUTRAL) {
