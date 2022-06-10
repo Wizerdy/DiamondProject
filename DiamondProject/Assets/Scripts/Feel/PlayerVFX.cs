5bffa033dc4c74783d01909ccdddf83de51b2185
Adrien Ribeiro <47816763+W3R10S@users.noreply.github.com>
2022-06-10 15:13:19 +0200

    Sound in scene FeelBoss_3

--- DiamondProject/Assets/Scripts/Feel/PlayerVFX.cs
+++ DiamondProject/Assets/Scripts/Feel/PlayerVFX.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 using MoreMountains.Feedbacks;
 
 public class PlayerVFX : MonoBehaviour {
+    [Header("References")]
     [SerializeField] EntityMovement _eMovement;
     [SerializeField] EntityMeleeAttack _eMeleeAttack;
     [SerializeField] Health _eTempHealth;
     [SerializeField] EntityRangedAttack _eRangedAttack;
+    [SerializeField] EntityChargeAttack _eChargeAttack;
+    [SerializeField] EntityChargeRanged _eChargeRange;
+
+    [Header("VFX")]
     [SerializeField] MMFeedbacks _acceleration;
     [SerializeField] MMFeedbacks _deceleration;
+    [SerializeField] MMFeedbacks _chargeAtk;
+    [SerializeField] MMFeedbacks _chargeAtkEnd;
+    [SerializeField] MMFeedbacks _chargingAtk;
+    [SerializeField] MMFeedbacks _chargeRange;
+    [SerializeField] MMFeedbacks _chargeRangeEnd;
+    [SerializeField] MMFeedbacks _chargingRange;
     [SerializeField] MMFeedbacks _meleeAttack;
     [SerializeField] MMFeedbacks _meleeHit;
     [SerializeField] MMFeedbacks _rangeAttack;
@@ -29,6 +40,12 @@ public class PlayerVFX : MonoBehaviour {
         _eTempHealth.OnHit += PlayHitFeedback;
         _eTempHealth.OnDeath += PlayDeathFeedback;
         _eRangedAttack.OnAttack += PlayRangedAttackFeedback;
+        _eChargeAttack.OnAttack += PlayChargeAttackFeedback;
+        _eChargeAttack.OnAttackEnd += PlayChargeAttackEndFeedback;
+        _eChargeAttack.OnCharging += PlayChargingAttackFeedback;
+        _eChargeRange.OnAttack += PlayChargeRangeFeedback;
+        _eChargeRange.OnAttackEnd += PlayChargeRangeEndFeedback;
+        _eChargeRange.OnCharging += PlayChargingRangeFeedback;
         //_eRangedAttack.OnHit += PlayRangedHitFeedback;
     }
 
@@ -41,7 +58,37 @@ public class PlayerVFX : MonoBehaviour {
         _eTempHealth.OnDeath -= PlayDeathFeedback;
         _eTempHealth.OnHit -= PlayHitFeedback;
         _eRangedAttack.OnAttack -= PlayRangedAttackFeedback;
+        _eChargeAttack.OnAttack -= PlayChargeAttackFeedback;
+        _eChargeAttack.OnAttackEnd -= PlayChargeAttackEndFeedback;
+        _eChargeAttack.OnCharging -= PlayChargingAttackFeedback;
+        _eChargeRange.OnAttack -= PlayChargeRangeFeedback;
+        _eChargeRange.OnAttackEnd -= PlayChargeRangeEndFeedback;
+        _eChargeRange.OnCharging -= PlayChargingRangeFeedback;
         //_eRangedAttack.OnHit -= PlayRangedHitFeedback;
+    }
+
+    private void PlayChargeAttackEndFeedback() {
+        _chargeAtkEnd.PlayFeedbacks();
+    }
+
+    private void PlayChargeRangeEndFeedback() {
+        _chargeRangeEnd.PlayFeedbacks();
+    }
+
+    private void PlayChargeRangeFeedback(Vector2 direction) {
+        _chargeRange.PlayFeedbacks();
+    }
+
+    private void PlayChargingRangeFeedback(Vector2 direction) {
+        _chargingRange.PlayFeedbacks();
+    }
+
+    private void PlayChargeAttackFeedback(Vector2 direction) {
+        _chargeAtk.PlayFeedbacks();
+    }
+
+    private void PlayChargingAttackFeedback(Vector2 direction) {
+        _chargingAtk.PlayFeedbacks();
     }
 
     private void PlayAccelerationFeedback(Vector2 direction) {
