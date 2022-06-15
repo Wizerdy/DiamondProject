bb88c3f06169bec9da3364a7a81dee822c2a5b28
Dadu <55183284+Darnald0@users.noreply.github.com>
2022-06-15 05:49:01 +0200

    add feedback in get hint

--- DiamondProject/Assets/Scripts/UI/GetHintFeedBack.cs
+++ DiamondProject/Assets/Scripts/UI/GetHintFeedBack.cs
@@ -7,12 +7,37 @@ public class GetHintFeedBack : MonoBehaviour
 {
     [SerializeField] private Image imageFeedBack;
     [SerializeField] private float fadeSpeed = 1.2f;
+    [SerializeField] private float duration = 5f;
+
+    private bool isInvisible = false;
+    private float transparancy = 1;
+    private float time = 5f;
+
+    private void OnEnable() {
+        time = duration;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float a = Mathf.PingPong(Time.deltaTime * fadeSpeed, 1);
-        Color color = new Color(imageFeedBack.color.r, imageFeedBack.color.g, imageFeedBack.color.b, a);
-        imageFeedBack.color = color;
+        if (time > 0) {
+            if (!isInvisible) {
+                transparancy -= Time.deltaTime * fadeSpeed;
+                Color color = new Color(imageFeedBack.color.r, imageFeedBack.color.g, imageFeedBack.color.b, transparancy);
+                imageFeedBack.color = color;
+                if (imageFeedBack.color.a <= 0.2f)
+                    isInvisible = true;
+            } else {
+                transparancy += Time.deltaTime * fadeSpeed;
+                Color color = new Color(imageFeedBack.color.r, imageFeedBack.color.g, imageFeedBack.color.b, transparancy);
+                imageFeedBack.color = color;
+                if (imageFeedBack.color.a >= 1)
+                    isInvisible = false;
+            }
+            time -= Time.deltaTime;
+        } else {
+            gameObject.SetActive(false);
+        }
+
     }
 }
