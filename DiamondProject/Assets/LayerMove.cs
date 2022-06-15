792013c1c8421b47caf1d7d28e5167d237baf5e9
W3R10S <adrien.ribeiro.me@gmail.com>
2022-06-15 19:24:13 +0200

    Mihcle.
    Jaqciue.

--- DiamondProject/Assets/LayerMove.cs
+++ DiamondProject/Assets/LayerMove.cs
@@ -7,15 +7,18 @@ public class LayerMove : MonoBehaviour {
     [SerializeField] float _distMax;
     [SerializeField] float _time;
     [SerializeField] AnimationCurve _smooth;
+
     float _dest = 0;
     Vector3 origin;
     Vector3 firstDestination;
     Vector3 destination;
+
     private void Start() {
         origin = transform.position;
         firstDestination = origin;
         destination = origin + new Vector3(Random.Range(-_distMax, _distMax), Random.Range(-_distMax, _distMax), 0);
     }
+
     public void Update() {
         _time = 1/(Vector3.Distance(firstDestination, destination) / _speed);
         _dest += Time.deltaTime * _time * _smooth.Evaluate(_dest);
