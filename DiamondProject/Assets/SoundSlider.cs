e3272a836f69a03be85f36d51b065994b8a27e02
W3R10S <adrien.ribeiro.me@gmail.com>
2022-06-16 16:18:57 +0200

    ça marche pas :3

--- DiamondProject/Assets/SoundSlider.cs
+++ DiamondProject/Assets/SoundSlider.cs
@@ -15,25 +15,47 @@ public class SoundSlider : MonoBehaviour {
     [SerializeField] TMP_Text _valueLabel;
     [SerializeField] TMP_Text _titleLabel;
     [SerializeField] UnityEvent<float> _onUpdate;
+    [SerializeField] GameObject menuPanel;
+
+    float oldValue = 100f;
 
     public event UnityAction<float> OnUpdate { add => _onUpdate.AddListener(value); remove => _onUpdate.RemoveListener(value); }
 
+    private void Awake() {
+        _slider.value = oldValue;
+        menuPanel.SetActive(false);
+    }
+
     private void Start() {
-        _onOffImage = _onOff.GetComponent<Image>();
+        _onOffImage.sprite = _onOff.GetComponent<Image>().sprite;
+        if (!_isActive) {
+            oldValue = _slider.value;
+            _slider.value = 0;
+            _slider.enabled = false;
+        } else {
+            _slider.value = oldValue;
+            _slider.enabled = true;
+        }
         UpdateSlider();
     }
 
     public void InverseState() {
         _isActive = !_isActive;
+        if (!_isActive) {
+            oldValue = _slider.value;
+            _slider.value = 0;
+            _slider.enabled = false;
+        } else {
+            _slider.value = oldValue;
+            _slider.enabled = true;
+        }
         UpdateSlider();
     }
 
     public void UpdateSlider() {
         _onOffImage.sprite = _isActive ? _onSprite : _offSprite;
         _valueLabel.text = _slider.value.ToString();
-        if (_isActive) {
-            _onUpdate?.Invoke(_slider.value);
-        }
+        _onUpdate?.Invoke(_slider.value);
     }
 
     public void SetRTPCValueGeneralVolume(float value) {
