019c4ee10b04ee02f987955bc03593e56a6de1d5
Rexmxxrayder <57299654+Rexmxxrayder@users.noreply.github.com>
2022-06-17 03:12:49 +0200

    layer

--- DiamondProject/Assets/OrderInLayerManager.cs
+++ DiamondProject/Assets/OrderInLayerManager.cs
@@ -7,26 +7,36 @@ public class OrderInLayerManager : MonoBehaviour {
     [SerializeField] Transform OffsetTransform;
     SpriteRenderer[] sr;
     MeshRenderer[] sk;
+    ParticleSystemRenderer[] ps;
     private void Start() {
         sr = GetComponents<SpriteRenderer>();
         sk = GetComponents<MeshRenderer>();
-        if (Offset) {
-            for (int i = 0; i < sr.Length; i++) {
-                sr[i].sortingOrder = (int)(OffsetTransform.position.y * -1000);
-            }
-            for (int i = 0; i < sk.Length; i++) {
-                sk[i].sortingOrder = (int)(OffsetTransform.position.y * -1000);
-            }
-        }
+        ps = GetComponents<ParticleSystemRenderer>();
+
     }
 
     private void Update() {
         if (!Offset) {
             for (int i = 0; i < sr.Length; i++) {
-                sr[i].sortingOrder = (int)(transform.position.y * -1000);
+                sr[i].sortingOrder = (int)(transform.position.y * -100);
             }
             for (int i = 0; i < sk.Length; i++) {
-                sk[i].sortingOrder = (int)(transform.position.y * -1000);
+                sk[i].sortingOrder = (int)(transform.position.y * -100);
+            }
+            for (int i = 0; i < ps.Length; i++) {
+                ps[i].sortingOrder = (int)(ps[i].gameObject.transform.position.y * -100);
+            }
+        } else {
+            for (int i = 0; i < sr.Length; i++) {
+                sr[i].sortingOrder = (int)(OffsetTransform.position.y * -100);
+                Debug.Log(OffsetTransform.position.y);
+                Debug.Log(OffsetTransform.position.y * -100);
+            }
+            for (int i = 0; i < sk.Length; i++) {
+                sk[i].sortingOrder = (int)(OffsetTransform.position.y * -100);
+            }
+            for (int i = 0; i < ps.Length; i++) {
+                ps[i].sortingOrder = (int)(OffsetTransform.position.y * -100);
             }
         }
     }
