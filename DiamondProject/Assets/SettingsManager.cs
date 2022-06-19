701c5c66108e8b19cf14bbc85785add4eb981c90
Rexmxxrayder <57299654+Rexmxxrayder@users.noreply.github.com>
2022-06-19 16:44:30 +0200

    +1

--- DiamondProject/Assets/SettingsManager.cs
+++ DiamondProject/Assets/SettingsManager.cs
@@ -6,10 +6,12 @@ public class SettingsManager : MonoBehaviour {
     [SerializeField] SoundSlider _generalSlider;
     [SerializeField] SoundSlider _musicSlider;
     [SerializeField] SoundSlider _sfxSlider;
+    [SerializeField] bool _mustclose;
     private void Start() {
         _generalSlider.Start();
         _musicSlider.Start();
         _sfxSlider.Start();
-        gameObject.SetActive(false);
+        if (_mustclose)
+            gameObject.SetActive(false);
     }
 }
