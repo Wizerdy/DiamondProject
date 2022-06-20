8d26358a4319d81012443f9c991823a68790aadf
Wizerdy <55747980+Wizerdy@users.noreply.github.com>
2022-06-20 17:39:54 +0200

    [MOD] IceWalls

--- DiamondProject/Assets/Scripts/Boss/Actions/IceWall.cs
+++ DiamondProject/Assets/Scripts/Boss/Actions/IceWall.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using ToolsBoxEngine;
 
+//[ExecuteInEditMode]
 [RequireComponent(typeof(LineRenderer))]
 public class IceWall : MonoBehaviour {
     [SerializeField] private int segments = 50;
@@ -13,10 +15,11 @@ public class IceWall : MonoBehaviour {
     //[SerializeField] private Reference<Transform> player;
 
     [SerializeField] private EdgeCollider2D edgeCollider;
-
     [SerializeField] private LineRenderer line;
+    [SerializeField] List<Transform> effects = new List<Transform>();
+
     private float circleAngle = 0;
-    [SerializeField] List<VisualEffects> effects = new List<VisualEffects>();
+    private Vector2[] _points;
 
     //private List<Vector2> colliderPoints = new List<Vector2>();
 
@@ -33,7 +36,11 @@ public class IceWall : MonoBehaviour {
 
         //float angle = Mathf.Atan2(player.Instance.position.x, player.Instance.position.y) * 180 / Mathf.PI;
         //transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, -angle);
-        CreateWall();
+        _points = new Vector2[segments + 1];
+        for (int i = 0; i < _points.Length; i++) {
+            _points[i] = new Vector2();
+        }
+        CreateWalls();
     }
 
     public void Init(int _segments, float _radius, float _gapWidth, float _speed, int _damage) {
@@ -45,60 +52,30 @@ public class IceWall : MonoBehaviour {
     }
 
     private void Update() {
-        float x;
-        float y;
-        circleAngle = 20f;
+        //Debug.DrawRay(Vector2.zero, Vector2.up * 100f, Color.red, 10f, false);
         radius -= speed * Time.deltaTime;
         if (radius <= 1) {
             Destroy(gameObject);
+            return;
         }
-        for (int i = 0; i < (segments + 1); i++) {
-            x = Mathf.Sin(Mathf.Deg2Rad * circleAngle) * radius;
-            y = Mathf.Cos(Mathf.Deg2Rad * circleAngle) * radius;
+        CreateWalls();
 
-            line.SetPosition(i, new Vector3(x, y, 0));
-
-            circleAngle += (360f - gapWidth) / segments;
-        }
-
+        SetGraphics();
         SetEdgeCollider();
-
-        //Vector3[] positions = GetPositions();
-
-        //if (positions.Length >= 2) {
-
-        //    int numberOfLines = positions.Length - 1;
-
-        //    polygonCollider.pathCount = numberOfLines;
-
-        //    for (int i = 0; i < numberOfLines; i++) {
-
-        //        List<Vector2> currentPositions = new List<Vector2> {
-        //            positions[i],
-        //            positions[i+1]
-        //        };
-
-        //        List<Vector2> currentColliderPoints = CalculateColliderPoints(currentPositions);
-        //        polygonCollider.SetPath(i, currentColliderPoints.ConvertAll(p => (Vector2)transform.InverseTransformPoint(p)));
-        //    }
-        //} else {
-
-        //    polygonCollider.pathCount = 0;
-        //}
     }
 
     private void SetEdgeCollider() {
         List<Vector2> edges = new List<Vector2>();
 
-        for (int i = 0; i < line.positionCount; ++i) {
-            Vector3 lineRendererPoint = line.GetPosition(i);
-            edges.Add(new Vector2(lineRendererPoint.x, lineRendererPoint.y));
+        for (int i = 0; i < _points.Length; ++i) {
+            Vector3 point = _points[i];
+            edges.Add(new Vector2(point.x, point.y));
         }
 
         edgeCollider.SetPoints(edges);
     }
 
-    private void CreateWall() {
+    private void CreateWalls() {
         float x;
         float y;
 
@@ -108,40 +85,33 @@ public class IceWall : MonoBehaviour {
             x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
             y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
 
-            line.SetPosition(i, new Vector3(x, y, 0));
+            //line.SetPosition(i, new Vector3(x, y, 0));
+            _points[i].Set(x, y);
 
             angle += (360f - gapWidth) / segments;
         }
     }
 
+    private void SetGraphics() {
+        Quaternion positiveAngle = Quaternion.AngleAxis(-gapWidth/2f, Vector3.forward);
+        Quaternion negativeAngle = Quaternion.AngleAxis(gapWidth/2f, Vector3.forward);
 
+        if (effects.Count < 8) { return; }
+        float dist = Tools.Remap(gapWidth, 0f, 180f, 1.25f, 0.5f);
 
-    //private List<Vector2> CalculateColliderPoints(List<Vector2> positions) {
-    //    float width = line.startWidth;
+        effects[0].localPosition = positiveAngle * Vector2.up * radius;
+        effects[1].localPosition = positiveAngle * new Vector2(dist, 1f) * radius;
+        effects[2].localPosition = new Vector2(dist, -1f) * radius;
+        effects[3].localPosition = Vector2.down * radius;
+        effects[4].localPosition = negativeAngle * Vector2.up * radius;
+        effects[5].localPosition = negativeAngle * new Vector2(-dist, 1f) * radius;
+        effects[6].localPosition = new Vector2(-dist, -1f) * radius;
+        effects[7].localPosition = Vector2.down * radius;
 
-    //    float m = (positions[1].y - positions[0].y) / (positions[1].x - positions[0].x);
-    //    float deltaX = (width / 2f) * (m / Mathf.Pow(m * m + 1, 0.5f));
-    //    float deltaY = (width / 2f) * (1 / Mathf.Pow(1 + m * m, 0.5f));
+        //Debug.DrawLine(Vector2.zero, result * 2f, Color.blue);
+        //Debug.DrawLine(Vector2.zero, result2 * 2f, Color.blue);
+    }
 
-    //    Vector2[] offsets = new Vector2[2];
-    //    offsets[0] = new Vector2(-deltaX, deltaY);
-    //    offsets[1] = new Vector2(deltaX, -deltaY);
-
-    //    List<Vector2> colliderPoints = new List<Vector2> {
-    //        positions[0] + offsets[0],
-    //        positions[1] + offsets[0],
-    //        positions[1] + offsets[1],
-    //        positions[0] + offsets[1]
-    //    };
-
-    //    return colliderPoints;
-    //}
-
-    //public Vector3[] GetPositions() {
-    //    Vector3[] pos = new Vector3[line.positionCount];
-    //    line.GetPositions(pos);
-    //    return pos;
-    //}
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             collision.gameObject.GetComponent<IHealth>()?.TakeDamage(damage);
